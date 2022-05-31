59f43e5e925ac29e862fce949b4c969b07e4330b
noname <48761044+noname0310@users.noreply.github.com>
2022-05-31 21:42:36 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{io, fs::File, time::Instant};
+use sha2::{Digest, Sha256};
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+    for entry in WalkDir::new("D:/noname") {
+        let entry = entry.unwrap();
+        let mut file = File::open(entry.path().as_os_str()).unwrap();
+        let mut sha256 = Sha256::new();
+        io::copy(&mut file, &mut sha256).unwrap();
+        let hash: [u8; 32] = sha256.finalize().into();
+        println!("hash is: {:?}", hash);
+        println!("{}", entry.path().display());
+    }
+    let duration = start.elapsed();
+    println!("end of search take {:?}", duration);
 }
